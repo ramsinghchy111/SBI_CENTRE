2719d1a2920f71ad102b13efd26a0ebdc59d1055
rams-demo <ramsinghchaudhry800@gmail.com>
2025-11-10 08:45:15 +0545

    header updated

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,5 +1,76 @@
 <template>
     <div class="hidden md:block">
-        <div class="bg-[#3d3028] w-full h-[180px] relative"></div>
+        <div class="bg-[#3d3028] w-full h-[180px] relative">
+            <div class="max-w-7xl mx-auto px-4 pt-6 flex justify-between items-center text-sm text-gray-300">
+                <div class="flex items-center space-x-3">
+                    <span>Have any questions? Email us</span>
+                    <div class="flex items-center space-x-1 font-semibold text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail w-4 h-4 text-[#e7a98b]" aria-hidden="true"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"></path><rect x="2" y="4" width="20" height="16" rx="2"></rect></svg>
+                        <a href="#" class="hover:underline">sbi.training.cntr@gmail.com</a>
+                    </div>
+                </div>
+                <div class="flex items-center space-x-6 text-gray-300">
+                    <a href="/navigation/about" class="hover:text-white transition-color">About</a>
+                    <a href="/navigation/gallery" class="hover:text-white transition-color">Success</a>
+                    <a href="/navigation/contact" class="hover:text-white transition-color">Emergency Call</a>
+                </div>
+            </div>
+        </div>
     </div>
+        <div class="absolute left-1/2 bottom-0 transform -translate-x-1/2 translate-y-1/2 w-full max-w-7xl px-4 sm:px-6px lg:px-8px ">
+            <div class="bg-white rounded-xl shadow-xl border border-gray-100 px-8 py-8">
+                <div class="flex flex-col md:flex-row md:justify-between md:items-center">
+                    <a href="" class="flex items-center mb-6 md:mb-0">
+                        <img src="/images/sbilogo.png" alt="" class="w-[120px] h-auto mr-3">
+                    </a>
+                    <div class="flex flex-col md:flex-row md:items-center md:space-x-12 text-center md:text-left">
+                        <div class="flex items-center justify-center space-x-3 mb-4 md:mb-0">
+                            <div class="w-14 h-14 rounded-full border border-orange-300 flex items-center justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone w-5 h-5 text-[#e7a98b]" aria-hidden="true"><path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"></path>
+                                </svg>
+                            </div>
+                            <div>
+                                <p class="text-sm italic text-gray-500">Phone number</p>
+                                <p class="text-lg font-bold text-gray-800">+977-9851403517</p>
+                            </div>
+                        </div>
+                        <div class="flex items-center justify-center space-x-3">
+                            <div class="w-14 h-14 rounded-full border border-orange-300 flex items-center justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin w-5 h-5 text-[#e7a98b]" aria-hidden="true"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"></path><circle cx="12" cy="10" r="3"></circle>
+                                </svg>
+                            </div>
+                            <div>
+                                <p class="text-sm italic text-gray-500">Address:</p>
+                                <p class="text-lg font-bold text-gray-800">Basundhara,Kathmandu,Nepal</p>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="border-t border-gray-200 my-6"></div>
+                <div class="flex justify-between items-center">
+                    <div class="flex items-center space-x-8">
+                        <div class="relative group">
+                            <a href="/" class="font-medium flex items-center space-x-1 transition-colors text-[#e7a98b]">Home</a>
+                        </div>
+                        <div class="relative group">
+                            <a href="/navigation/about" class="font-medium flex items-center space-x-1 transition-colors text-gray-800 hover:text-[#e7a98b]">About Us</a></div>
+                        <div class="relative group">
+                            <a href="/navigation/about" class="font-medium flex items-center space-x-1 transition-colors text-gray-800 hover:text-[#e7a98b]">Services</a>
+                        </div>
+                        <div class="relative group">
+                            <a href="/navigation/about" class="font-medium flex items-center space-x-1 transition-colors text-gray-800 hover:text-[#e7a98b]">FAQ'S</a>
+                        </div>
+                        <div class="relative group">
+                            <a href="/navigation/about" class="font-medium flex items-center space-x-1 transition-colors text-gray-800 hover:text-[#e7a98b]">Gallery</a>
+                        </div>
+                        <div class="relative group">
+                            <a href="/navigation/about" class="font-medium flex items-center space-x-1 transition-colors text-gray-800 hover:text-[#e7a98b]">Contact Us</a>
+                        </div>
+                    </div>
+                    <div class="flex space-x-4">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
 </template>
